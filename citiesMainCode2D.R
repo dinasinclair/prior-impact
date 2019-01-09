66f12a3e19b705b8abbfcf65efcc0ec620f79d61
dinasinclair <dinacrs@gmail.com>
2019-01-08 21:51:08 -0800

    finished first round of commenting citiesMainCode2D

--- citiesMainCode2D.R
+++ citiesMainCode2D.R
@@ -3,6 +3,7 @@
 # TODO(dsinc): separate into different files
 # TODO(dsinc): add tests
 # TODO(dsinc): decide on sd vs tau conventions plus make sure we're not mixing sd vs var
+# TODO(dsinc): call it K or call it num_final_cities?
 
 
 library("rstan")
@@ -30,7 +31,7 @@ generateGroupedData <- function(I, mu, sd, N, SF=1){
   for (i in 1:I){
     mu_studies[i] <- rnorm(1,mean = G$mu[group_assignment[i]], sd = G$sd[group_assignment[i]])
   }
-  Y <- list(mu = mu_studies, sd = sd_studies)
+  Y <- list(mu = mu_studies, sd = sd_studies)rm(list = ls())
   
   # Save our generated input data together in a list
   generated_data <- list(I = I, N = N, Y = Y, G = G, groups = group_assignment)
@@ -50,7 +51,7 @@ parseGeneratedGroupedData <- function(generated_data){
   return(stan_input_data_format)
 }
 
-extract_fit <- function(data){
+extractFit <- function(data){
   # This function returns the parameters backed out from the stan REM model. 
   # TODO(dsinc): should this instead be a function that takes in a bunch of arguments rather than one list?
   #
@@ -69,19 +70,22 @@ extract_fit <- function(data){
   Y <- list(mean = data$Y_mean, sd = data$Y_sd)
   G<- list(mean = data$G_mean, sd = data$G_sd)
   
+  # Update values of Y, G, mu, tau appropriately
   for (i in 1:length(data$Y_mean)){
     Y$mean[i] <- mean(params$theta[,i])
   }
-  for (n in 1:length(data$groups)){
+  for (n in 1:length(data$G_mean)){
     G$mean[n] <- mean(params$G_mean[,n])
     G$sd[n] <- mean(params$G_sd[,n])
   }
+  mu = mean(params$mu)
+  tau = mean(params$tau)
   
-  return(list(Y = Y, G = G, mu = mean(params$mu), tau = mean(params$tau)))
+  return(list(Y = Y, G = G, mu = mu, tau = tau))
 }
 
 
-get_pilot_results <- function(K,Y,Q=1){
+getPilotResults <- function(K,Y,Q=1){
   # Calculate new data for all K new pilots
   # 
   # Arguments:
@@ -99,40 +103,63 @@ get_pilot_results <- function(K,Y,Q=1){
     new_mean <- rnorm( 1, mean = Y$mean[k], sd = new_sd)
     
     # Combine the old and new data 
-    post_pilot <- update_Y(Y$mean[k], new_mean, Y$sd[k], new_sd)
+    post_pilot <- combineMeanAndVar(mu1 = Y$mean[k], mu2 = new_mean, var1 = (Y$sd[k])^2, var2 = new_sd^2)
     Y_P$mean[k] <- post_pilot$mean
-    Y_P$var[k] <- post_pilot$var
+    Y_P$sd[k] <- sqrt(post_pilot$var)
     
   }
   return(Y_P)
 }
 
-update_Y <- function(mu1, mu2, sigSq1, sigSq2){
-  update_mean  <- (mu1*sigSq2 + mu2*sigSq1)/(sigSq1 + sigSq2)
-  update_var <- (sigSq1*sigSq2)/(sigSq1 + sigSq2)
-  return(list(mean = update_mean , var = update_var))
+combineMeanAndVar <- function(mu1, mu2, var1, var2){
+  # Combines the means and variances of two data points (mu1, var1) (mu2, var2)
+  
+  combined_mean  <- (mu1*var2 + mu2*var1)/(var1 + var2)
+  combined_var <- (var1*var2)/(var1 + var2)
+  
+  return(list(mean = combined_mean , var = combined_var))
 }
 
-new_ranking <- function(fit_updated,Y_P) {
-  params_updated <- extract(fit_updated)
-  Y_updated <- Y_P
-  for (i in 1:length(Y_updated$mean)){
-    Y_updated$mean[i] <- mean(params_updated$theta[,i])
-  }
-  new_rank <- order(Y_updated$mean, decreasing=TRUE)
-  return(new_rank)
-}
+# getNewRanking <- function(fit_updated,Y_P) {
+#   # Returns a new ranking of programs by quality in decreasing order
+#   # Arguments:
+#   #   fit_updated: fit after being updated by TODO(dsinc): pilot? something? double check
+#   #   Y_P: Y after being updated by the pilot information
+#   #
+#   # Note: Not actually used right now in the main function, so I'm commenting it out
+#   
+#   params_updated <- extract(fit_updated)
+#   Y_updated <- Y_P
+#   for (i in 1:length(Y_updated$mean)){
+#     Y_updated$mean[i] <- mean(params_updated$theta[,i])
+#   }
+#   new_rank <- order(Y_updated$mean, decreasing=TRUE)
+#   return(new_rank)
+# }
 
-change_mind <- function(K,Y,original_rank,num_final_cities,Q=1){
-  # This function returns TRUE if the pilots run in K change the final actions
-  # and returns FALSE otherwise, given original ranking and data Y.
+doPilotsChangeOurMinds <- function(K,Y,original_rank,num_final_cities,Q=1){
+  # Returns true if the pilots run in K change the final actions
+  # and returns false otherwise, given original ranking and data Y.
+  # 
+  # Arguments:
+  #   K: number of pilots (additional samples) run
+  #   Y: set of datapoints with (mean, sd) per datpoint
+  #   original_rank: int list ordering the Y datapoints from best (highest mean)
+  #     to worst (lowest mean) with 1 being best
+  #   num_final_cities: number of datapoints (cities) chosen in final program
+  #   Q: factor by which pilot variance is different than original Y variance
+  #   
+  # Returns:
+  #   True if the top num_final_cities are different in the original_rank vs
+  #   the new rank post K cities getting additional pilots, false otherwise
+    
   
   # Simulate pilots
-  Y_P <- get_pilot_results(K,Y,Q)
+  Y_P <- getPilotResults(K,Y,Q)
   
   # Update thetas
   updated_data <- list(I=length(Y$mean), Y=Y_P$mean, sigmaSq=Y_P$var)
-  Y_updated <- extract_fit(updated_data)$Y
+  Y_updated <- extractFit(updated_data)$Y
   
   # Use new thetas to get a new city ranking
   new_rank <- order(Y_updated$mean, decreasing=TRUE)
@@ -145,10 +172,22 @@ change_mind <- function(K,Y,original_rank,num_final_cities,Q=1){
   return(!setequal(original_choice,new_choice))
 }
 
-overall<-function(data,num_pilots,num_final_cities,num_draws,Q=1){
+overall<-function(data, num_pilots, num_final_cities, num_draws, Q=1){
+  # Calculates the frequency at which every possible pilot set changes our minds.
+  # 
+  # Arguments:
+  #   data: list(I,N,Y_mean,Y_sd,groups) likely generated from parseGeneratedGroupedData
+  #   num_pilots: number of cities we
+  #   num_final_cities: number of datapoints (cities) chosen in final program (same as K)
+  #   num_draws: number of times we simulate changing our minds TODO(dsinc): better name for this?
+  #   Q: factor by which pilot variance is different than original Y variance
+  #   
+  # Returns:
+  #   True if the top num_final_cities are different in the original_rank vs
+  #   the new rank post K cities getting additional pilots, false otherwise
   
   # Do initial REM fit
-  Y <- extract_fit(data)$Y
+  Y <- extractFit(data)$Y
   
   # Calculate the city ranking of the original data
   original_rank <- order(Y$mean, decreasing=TRUE)
@@ -163,7 +202,7 @@ overall<-function(data,num_pilots,num_final_cities,num_draws,Q=1){
   for (i in 1:ncol(combinations)){
     for (j in 1:num_draws){
       K <- combinations[,i]
-      nmc[i] <- nmc[i] + change_mind(K,Y,original_rank,num_final_cities)
+      nmc[i] <- nmc[i] + doPilotsChangeOurMinds(K,Y,original_rank,num_final_cities)
     }
   } 
   
@@ -171,8 +210,10 @@ overall<-function(data,num_pilots,num_final_cities,num_draws,Q=1){
 }
 
 data <- parseGeneratedGroupedData(generateGroupedData(I=10,mu=0,sd=10,N=3, SF=1))
-fit <- stan(file = 'randomEffectsModel2D.stan', 
-            data = data, 
-            iter = 1000, chains = 2)
+# fit <- stan(file = 'randomEffectsModel2D.stan', 
+#             data = data, 
+#             iter = 1000, chains = 2)
+# 
+# extractFit(data)
 
-extract_fit(data)
+overall(data,3,2,4,Q=1)
